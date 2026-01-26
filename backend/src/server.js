1c01bf05f9480ca96a4f446336ba36990970980b
Shantanu <shantanuband98@gmail.com>
2026-01-26 07:37:26 +0200

    Remove frontend serving from backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,23 +9,22 @@ import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import { app, server } from "./lib/socket.js";
 
-app.set("trust proxy", 1); // 🔥 REQUIRED FOR RENDER COOKIES
-
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json({ limit: "5mb" }));
+app.use(cookieParser());
+
 app.use(
   cors({
     origin: ENV.CLIENT_URL,
     credentials: true,
   })
 );
-app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-server.listen(PORT, () => {
-  console.log("Server running on port: " + PORT);
-  connectDB();
+server.listen(PORT, async () => {
+  console.log("Server running on port:", PORT);
+  await connectDB();
 });
